bbc8f7dd4029e208d342be7de5278d63a043a9f9
Justin Sitter <justin@jaytria.com>
2018-03-21 21:11:55 -0700

    cursor movements

---
+++ game.js
@@ -0,0 +1,102 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    
+    this.load.setBaseURL('./');
+
+    this.load.image('board', 'board.jpg');
+    this.load.spritesheet('loris', 'slow-loris.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('red', 'slow-loris.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'board');
+    this.add.image(32, 48, 'red')
+   
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'red').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'red');
+    platforms.create(50, 250, 'red');
+    platforms.create(750, 220, 'red');
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    player = this.physics.add.sprite(50, 8, 'loris');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('loris', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'loris', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('loris', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    // logo.setVelocity(100,50);
+    // logo.setBounce(1, .98);
+    // logo.setCollideWorldBounds(true);
+
+    // emitter.startFollow(logo);
+}
+
+function update()
+{
+    if (cursors.left.isDown)
+{
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+}
+else if (cursors.right.isDown)
+{
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+}
+else
+{
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+}
+
+if (cursors.up.isDown && player.body.touching.down)
+{
+    player.setVelocityY(-330);
+}
+}
