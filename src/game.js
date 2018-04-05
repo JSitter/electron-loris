89843b3b31f7aee6d9a3d637f1ecde53b22e0a49
Justin Sitter <justin@jaytria.com>
2018-04-05 14:12:25 -0700

    evertyhing is ready

--- src/game.js
+++ src/game.js
@@ -1,37 +1,36 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-import 'phaser';
 
-var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
 
-var game = new Phaser.Game(config);
 
-function preload ()
-{
-    this.load.image('logo', 'assets/slow-loris.png');
-}
+// var config = {
+//     type: Phaser.AUTO,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     scene: {
+//         preload: preload,
+//         create: create
+//     }
+// };
 
-function create ()
-{
-    var logo = this.add.image(400, 150, 'logo');
+// var game = new Phaser.Game(config);
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
+// function preload ()
+// {
+//     this.load.image('logo', 'assets/slow-loris.png');
+// }
 
-}
+// function create ()
+// {
+//     var logo = this.add.image(400, 150, 'logo');
+
+//     this.tweens.add({
+//         targets: logo,
+//         y: 450,
+//         duration: 2000,
+//         ease: 'Power2',
+//         yoyo: true,
+//         loop: -1
+//     });
+
+// }
+alert("yup")
