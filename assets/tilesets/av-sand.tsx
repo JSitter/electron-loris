20e267fff40d40627ebf3aec9e50294c330f1ba3
Justin Sitter <justin@jaytria.com>
2018-04-05 19:00:20 -0700

    Keeping up with the Andersons

--- assets/tilesets/av-sand.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="av-sand" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="../art/contrib/avalon/beach_sand_woa.png" trans="ff00ff" width="174" height="114"/>
-</tileset>
