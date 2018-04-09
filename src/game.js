a7dba8d50736b59b2714b1d693bcf15241802384
Justin Sitter <justin@jaytria.com>
2018-04-08 17:32:54 -0700

    commented out sample code

--- src/game.js
+++ src/game.js
@@ -139,11 +139,12 @@ sceneOne.create = function(){
         repeat: -1
     });
 
-    stars = this.physics.add.group({
-        key: 'star',
-        repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
+    //  Example for adding sprites
+    // stars = this.physics.add.group({
+    //     key: 'star',
+    //     repeat: 11,
+    //     setXY: { x: 12, y: 0, stepX: 70 }
+    // });
     
     stars.children.iterate(function (child) {
     
